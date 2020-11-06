a3b96462dc432a7a62c4457b10393f56a2eee51b
ravk1 <ravk1@LAPTOP-R3I7J7LA.lan1>
2020-11-05 20:46:37 -0600

    .

--- src/main/java/com/acti/base/DriverScript.java
+++ src/main/java/com/acti/base/DriverScript.java
@@ -1,5 +1,75 @@
 package com.acti.base;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+/*
+ * Name: DriverScript
+ * Description: XXXXXXXXX
+ * Developed By: Your Name
+ * Date Modified: 11/04/2020
+ */
+
 public class DriverScript {
+	
+	public static WebDriver driver;
+	static Properties prop;
+	
+	public DriverScript()
+	{
+		try
+		{
+			File file = new File("./src/test/resources/config/config.properties");
+			FileInputStream fis = new FileInputStream(file);
+			prop = new Properties();
+			prop.load(fis);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Unable to load the file "+e.getMessage());
+		}
+	}
+	@Test
+	public static void initBrowser()
+	{
+		String browser = prop.getProperty("browser");
+		if(browser.equalsIgnoreCase("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", "./src/test/resources/browsers/chromedriver.exe");
+			driver=new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver", "./src/test/resources/browsers/geckodriver.exe");
+			driver=new FirefoxDriver();
+		}
+		else
+		{
+			System.out.println("Unsupported browser name");
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);	
+	}
+	
+	public static void launchApplication()
+	{
+		String url = prop.getProperty("qaurl");
+		driver.get(url);
+	}
+	
+	public static void closeApplication()
+	{
+		driver.quit();
+	}
 
 }
--- src/main/java/com/acti/pages/LoginPage.java
+++ src/main/java/com/acti/pages/LoginPage.java
@@ -3,6 +3,6 @@ package com.acti.pages;
 import com.acti.base.DriverScript;
 
 public class LoginPage extends DriverScript {
-  //sameer updated his changes and checked in
+ 
 
 }
